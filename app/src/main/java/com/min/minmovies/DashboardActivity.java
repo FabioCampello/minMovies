c61b37ee8aad109029e2ba331079320a0c3d1be4
FabioCampello <fabiocampello@outlook.com.br>
2020-03-14 19:58:50 -0300

    Adicionado toolbar.

--- app/src/main/java/com/min/minmovies/DashboardActivity.java
+++ app/src/main/java/com/min/minmovies/DashboardActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,41 +18,35 @@ public class DashboardActivity extends AppCompatActivity {
     private FirebaseAuth auth;
     private FirebaseUser user;
 
-    private TextView edt_email;
-    private TextView edt_id;
     private Button btn_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
-        inicializaComponentes();
-        eventosDeClick();
+//        inicializaComponentes();
+//        eventosDeClick();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        edt_email.setText("");
-        edt_id.setText("");
+
         auth = Conexao.getFirebaseAuth();
         user = Conexao.getFirebaseUser();
         verificarUsuario();
     }
 
     private void inicializaComponentes() {
-        edt_email = (TextView) findViewById(R.id.edtEmail);
-        edt_id = (TextView) findViewById(R.id.edtId);
         btn_logout = (Button) findViewById(R.id.btnLogout);
     }
 
     private void verificarUsuario() {
         if(user == null) {
             finish();
-        } else {
-            edt_email.setText(user.getEmail());
-            edt_id.setText(user.getUid());
         }
     }
 
@@ -62,8 +57,6 @@ public class DashboardActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Conexao.logout();
-                edt_email.setText("");
-                edt_id.setText("");
                 finish();
             }
         });
